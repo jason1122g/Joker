edd2cef3011307cc9b3a54685ec2d7eca9b24ba8
jason1122g <jason1122g@gmail.com>
2014-07-31 23:25:03 +0800

    Update to correct behavior

--- src/main/groovy/org/joker/component/event/DragEvent.java
+++ src/main/groovy/org/joker/component/event/DragEvent.java
@@ -5,14 +5,15 @@ import org.joker.component.JokerComponent;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class DragEvent implements MouseMotionListener{
 
-    private Set<JokerComponent> components = new HashSet<JokerComponent>();
-    private Point anchorPoint;
+    private Set<JokerComponent>       components        = new HashSet<JokerComponent>();
+    private Map<JokerComponent,Point> beforeDragPointMap = new HashMap<JokerComponent, Point>();
+    private Point targetMovePoint;
+    private Point beforeDragPoint;
+    private boolean isFirstDrag;
 
     public DragEvent with(JokerComponent...components){
         Collections.addAll(this.components,components);
@@ -32,23 +33,53 @@ public class DragEvent implements MouseMotionListener{
 
     @Override
     public void mouseMoved(MouseEvent e) {
-        anchorPoint = e.getPoint();
+        if(targetMovePoint != e.getPoint()){
+            clearDragCondition();
+        }
+        targetMovePoint = e.getPoint();
+        beforeDragPoint = e.getComponent().getLocation();
+    }
+
+    private void clearDragCondition(){
+        isFirstDrag = true;
+        beforeDragPointMap.clear();
     }
 
     @Override
     public void mouseDragged(MouseEvent e) {
-        int anchorX = anchorPoint.x;
-        int anchorY = anchorPoint.y;
+        tryInitDragCondition();
+        setNextLocationOfAll(e);
+    }
 
-        for(JokerComponent component : components){
-            Point parentOnScreen = component.getParent().getLocationOnScreen();
-            Point mouseOnScreen  = e.getLocationOnScreen();
-
-            int nextX = mouseOnScreen.x - parentOnScreen.x - anchorX;
-            int nextY = mouseOnScreen.y - parentOnScreen.y - anchorY;
-
-            component.setLocation( new Point(nextX, nextY) );
+    private void tryInitDragCondition(){
+        if(isFirstDrag){
+            for(JokerComponent component : components){
+                beforeDragPointMap.put(component, component.getLocation());
+            }
+            isFirstDrag = false;
         }
+    }
+
+    private void setNextLocationOfAll(MouseEvent e){
+        Point nextRelativePoint = getNextRelativePoint(e);
+        for(JokerComponent component : components){
+            Point originPos = beforeDragPointMap.get(component);
+            int   nextX     = originPos.x + nextRelativePoint.x;
+            int   nextY     = originPos.y + nextRelativePoint.y;
+            component.setLocation(nextX,nextY);
+        }
+    }
+
+    private Point getNextRelativePoint(MouseEvent e){
+
+        Point parentOnScreen = e.getComponent().getParent().getLocationOnScreen();
+        Point mouseOnScreen  = e.getLocationOnScreen();
+
+        int nextX = mouseOnScreen.x - parentOnScreen.x - targetMovePoint.x - beforeDragPoint.x;
+        int nextY = mouseOnScreen.y - parentOnScreen.y - targetMovePoint.y - beforeDragPoint.y;
+
+        return new Point(nextX,nextY);
+
     }
 
 }
